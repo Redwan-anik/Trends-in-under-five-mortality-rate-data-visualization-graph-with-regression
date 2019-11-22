93a7b045389945b5793b809174f47f44611293f9
Md.Redwanul haque <32310502+Redwan-anik@users.noreply.github.com>
2019-11-23 03:12:24 +0600

    Add files via upload

---
+++ reg.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+df = pd.read_csv("x.csv")
+plt.title("Trends in under-five mortality rate")
+plt.xlabel("Year")
+plt.ylabel("Rate(PER 1,000 LIVE BIRTHS)")
+plt.plot(df.Year,df.Rate,color ='blue', marker ='o')
+reg = linear_model.LinearRegression()
+reg.fit(df[['Year']],df.Rate)
+p=reg.predict(2031)
+print("Prediction =",p)   
